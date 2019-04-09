46629916992107c6d618a2278566d7058beaad05
Rafael Albarello <rafael.albarello@trinovati.com.br>
2019-04-09 12:02:40 -0300

    Template for receiving outside public key

--- x25519.py
+++ x25519.py
@@ -1,14 +1,16 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PublicKey
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import serialization
 
-
+#Message received via UDP from Arduino, the Public Key
+msg = '802485554bee3688f8e33567b304080857cabd313e6412fdf610d56037359148'
 
 # Generate a private key for use in the exchange.
-#private_key = X25519PrivateKey.generate()
-private_key_bytes = bytes.fromhex('802485554bee3688f8e33567b304080857cabd313e6412fdf610d56037359148')
+private_key = X25519PrivateKey.generate()
+#private_key_bytes = bytes.fromhex('802485554bee3688f8e33567b304080857cabd313e6412fdf610d56037359148')
 private_key = X25519PrivateKey.from_private_bytes(
     data = private_key_bytes
 )
@@ -35,44 +37,14 @@ print("Chave Publica a")
 print(chave)
 
 ##Chave Bob
+arduino_public_key_bytes = bytes.fromhex(msg)
+arduino_public_key = X25519PublicKey.from_public_bytes(arduino_public_key_bytes)
 
-private_keyb = X25519PrivateKey.generate()
-
-private_bytes = private_keyb.private_bytes(
-    encoding=serialization.Encoding.Raw,
-    format=serialization.PrivateFormat.Raw,
-    encryption_algorithm=serialization.NoEncryption()
-)
-
-#chave = ", 0x".join("{:02x}".format(ord(c)) for c in private_bytes)
-chave = private_bytes.hex()
-print("Chave Privada b")
-print(chave)
-
-public_keyb = private_keyb.public_key()
-public_bytes = public_keyb.public_bytes(
-    encoding=serialization.Encoding.Raw,
-    format=serialization.PublicFormat.Raw
-)
-
-#chave = ", 0x".join("{:02x}".format(ord(c)) for c in public_bytes)
-chave = public_bytes.hex()
-print("Chave Publica b")
-print(chave)
 
 #chave compartilhada A
-shared_key = private_key.exchange(public_keyb)
+shared_key = private_key.exchange(arduino_public_key)
 
 #chave = ", 0x".join("{:02x}".format(ord(c)) for c in shared_key)
 chave = shared_key.hex()
 print("Chave compartilhada a")
 print(chave)
-
-#chave compartilhada b
-shared_key = private_keyb.exchange(public_key)
-
-#chave = ", 0x".join("{:02x}".format(ord(c)) for c in shared_key)
-chave = shared_key.hex()
-print("Chave compartilhada a")
-print(chave)
-
